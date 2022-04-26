d9e082ba44cc3f3fd441a4954d7099160089c8bc
Animaline <thetanguy@gmail.com>
2022-04-26 19:59:42 +0200

    users database

--- showtime-back/src/users/users.module.ts
+++ showtime-back/src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { users, usersSchema } from 'src/schemas/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: users.name, schema: usersSchema }]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
