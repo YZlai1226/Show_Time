21ae1fc85d9c83c8d0ac30ba927c45edf7aebfa7
Animaline <thetanguy@gmail.com>
2022-04-28 19:16:56 +0200

    auth_v3_quasi_fini

--- showtime-back/src/app.controller.ts
+++ showtime-back/src/app.controller.ts
@@ -1,24 +1,39 @@
 // import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 // import { AuthGuard } from '@nestjs/passport';
-// import { LocalAuthGuard } from './auth/local-auth.guard';
+// import { Users } from './schemas/users.schema';
+// import { AuthService } from './auth/auth.service';
+
 // @Controller()
 // export class AppController {
-//   @UseGuards(LocalAuthGuard)
-//   @Post('auth/login')
-//   async login(@Request() req) {
-//     return req.user;
+//   constructor(private authService: AuthService) { }
+
+//     @UseGuards(AuthGuard('local'))
+//     @Post('auth/login')
+//     async login(@Request() req): Promise<Users> {
+//       // console.log(req.user._doc)
+//       return req.user._doc;
+//     }
 //   }
-// }
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Users } from './schemas/users.schema';
+
+
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req): Promise<Users> {
-    // console.log(req.user._doc)
-    return req.user._doc;
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
