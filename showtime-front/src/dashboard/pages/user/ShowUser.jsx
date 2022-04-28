acc09be9fb6a6f197ee52045ec791654d8725aaf
AnisZouhri <anis.zouhri@epitech.eu>
2022-04-28 12:37:23 +0200

    A12

--- showtime-front/src/dashboard/pages/user/ShowUser.jsx
+++ showtime-front/src/dashboard/pages/user/ShowUser.jsx
@@ -11,8 +11,14 @@ import axios from 'axios';
 export default function ShowUser() {
 
     let { userId } = useParams();
-    const [user, setUserData] = useState({})
+    const [user, setUserData] = useState({});
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+    const [userActive, setUserActive] = useState('');
+    const [userAdmin, setUserAdmin] = useState('');
 
+    // Get user 
     useEffect(() => {
       axios.get(`http://localhost:3000/users/${userId}`)
       .then(res => {
@@ -22,26 +28,37 @@ export default function ShowUser() {
       .catch(err => {
         console.log(err)
       })
-    }, [userId])
+    }, [userId]);
 
+    const editUser =  (e) => {
+        e.preventDefault();
+        const updatedUser = { name: userName, email: userEmail, 
+            password: userPassword, accounte_active: userActive, admin: userAdmin };
+        axios.put(`http://localhost:3000/users/${userId}`, updatedUser);
+        setUserName('');
+        setUserEmail('');
+        setUserPassword('');
+        setUserActive('');
+        setUserAdmin('');   
+
+    }
     
   return (
     <>
         <Topbar />
         <Container style={{marginTop: 100}}>
-            <h1> Edit </h1>
             <Row>
-                <Col >
-                    <div className='user'> 
-                         <h6>User n° {userId}</h6>
+                <Col lg= "6">
+                    <div className=''  > 
+
                          <Card sx={{ maxWidth: 500 }}>
-                            <CardMedia sx={{borderRadius: 50, width: 250, height: 250, marginLeft: 15,}}
+                            <CardMedia sx={{borderRadius: 50, width: 250, height: 250, marginLeft: 15, marginBottom: 1, marginTop: 5,}}
                                 component="img"
                                 height="140"
                                 image= {user.avatar}
                                 alt=""
                             />
-                            <CardContent>
+                            <CardContent sx={{marginBottom: 5}} >
                                 <Typography gutterBottom variant="h5" component="div">
                                 Name: {user.name}
                                 </Typography>
@@ -55,10 +72,10 @@ export default function ShowUser() {
                                 Admin: {user.admin}
                                 </Typography>
                                 <Typography variant="h6" color="text.secondary">
-                                Created_at: {user.updated_at}
+                                Created : {user.updated_at}
                                 </Typography>
                                 <Typography variant="h6" color="text.secondary">
-                                Updated_at: {user.created_at}
+                                Updated : {user.created_at}
                                 </Typography>
 
                             </CardContent>
@@ -66,26 +83,34 @@ export default function ShowUser() {
 
                     </div>
                 </Col>
-                <Col>
-                <Form>
+                <Col lg= "6">
+                <div className='shadow-sm p-3 mb-5 bg-white rounded' style={{marginTop: 50}}>
+                        <h3> Edit user n° {userId}</h3>
+                <Form onSubmit={editUser}>
                     <Form.Group className="mb-3" controlId="formBasicA">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="New name.." />
+                    <Form.Control value={userName} type="text" placeholder= {user.name} />
                     </Form.Group>
                    
                     <Form.Group className="mb-3" controlId="formBasicA">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="text" placeholder="New Email.." />
-                    </Form.Group>
-                   
-                    <Form.Group className="mb-3" controlId="formBasicA">
-                    <Form.Label>Actif</Form.Label>
-                    <Form.Control type="text" placeholder="Actif.." />
+                    <Form.Control value={userEmail} type="text" placeholder= {user.email} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicA">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control value={userPassword} type="text" placeholder= {user.password} />
+                    </Form.Group>
+
+                   
+                    <Form.Group className="mb-3" controlId="formBasicC">
+                    <Form.Label>Actif</Form.Label>
+                    <Form.Control value={userActive} type="text" placeholder= {user.account_active} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicD">
                     <Form.Label>Admin right</Form.Label>
-                    <Form.Control type="text" placeholder="Admin right...." />
+                    <Form.Control value={userAdmin} type="text" placeholder= {user.admin} />
                     </Form.Group>
 
                     <Button className='mt-2' variant="primary" type="submit">
@@ -93,7 +118,7 @@ export default function ShowUser() {
                     </Button>
 
                 </Form>
-
+                </div>
                 </Col>
             </Row>
         
