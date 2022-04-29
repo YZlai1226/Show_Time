37e74f6dcd8eee0c2fe360450a658c23d4df18bd
Karen <yong-zhen.lai@epitech.eu>
2022-04-29 12:26:57 +0200

    fix conflict in main

--- showtime-back/src/main.ts
+++ showtime-back/src/main.ts
@@ -5,13 +5,9 @@ import { PassportModule } from '@nestjs/passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-  app.enableCors();
-=======
 
   app.enableCors();
 
->>>>>>> 97ea644f182c478b523d7d192569f7ab5719f803
   await app.listen(3000);
 }
 bootstrap();
