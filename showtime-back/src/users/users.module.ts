e489264daf6da5289b44ed7554d727e5060febd8
Karen <yong-zhen.lai@epitech.eu>
2022-04-29 17:27:06 +0200

    change the way to get one user info

--- showtime-back/src/users/users.module.ts
+++ showtime-back/src/users/users.module.ts
@@ -3,9 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Users, UsersSchema } from 'src/schemas/users.schema';
+import { BookingsService } from '../bookings/bookings.service';
+import { BookingsModule } from '../bookings/bookings.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]), BookingsModule ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
